0733f9e12e054a5efe9aa75f54da44e8167bc96a
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2025-02-17 18:10:20 +0000

    basic setup, shadcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import ThemeToggle from '@/components/shared/theme-toggle';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className='font-bold text-blue-500'>Welcome to the Course</h1>
+    <div className='p-5'>
+      <div className='w-100 flex justify-end'>
+        <ThemeToggle />
+      </div>
+      <h1 className='font-bold text-blue-500 font-barlow'>
+        Welcome to the Course
+      </h1>
+      <Button variant='destructive'>Click me</Button>
     </div>
   );
 }
---
+++ src/components/shared/theme-toggle.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { MoonIcon, SunIcon } from 'lucide-react';
+import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+
+export default function ThemeToggle() {
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme') || 'system';
+    setTheme(savedTheme);
+    applyTheme(savedTheme);
+  }, [setTheme]);
+
+  const applyTheme = (newTheme: string) => {
+    if (newTheme === 'dark') {
+      document.documentElement.classList.add('dark');
+      document.documentElement.style.colorScheme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.style.colorScheme = 'light';
+    }
+  };
+
+  const handleThemeChange = (newTheme: string) => {
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+    applyTheme(newTheme);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='outline'
+          size='icon'
+          className='w-10 h-10 rounded-full'
+        >
+          <SunIcon className='h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <MoonIcon className='absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          <span className='sr-only'>Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem onClick={() => handleThemeChange('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeChange('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeChange('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
